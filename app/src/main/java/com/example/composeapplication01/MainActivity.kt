6f656fe5defc89f4257e229da8c4e8c3a806359c
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-11-06 00:48:00 +0200

    Added a text composable function

--- app/src/main/java/com/example/composeapplication01/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication01/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.composeapplication01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
+            //Add a text composable
+            Text(text = "JETPACK COMPOSE APP")
 
         }
     }
